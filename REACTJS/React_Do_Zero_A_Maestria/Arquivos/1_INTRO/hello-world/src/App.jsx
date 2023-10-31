3c2a5675bbef7aa418d19c555bc7a57e87336ba1
NFPereira <nilsonfpereira@hotmail.com>
2023-10-31 08:27:00 -0300

    Tarefa 1 - Desafio React

--- REACTJS/React_Do_Zero_A_Maestria/Arquivos/1_INTRO/hello-world/src/App.jsx
+++ REACTJS/React_Do_Zero_A_Maestria/Arquivos/1_INTRO/hello-world/src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <div className='App'>
       <h1>Hello World</h1>
+      <h2>Emmet funcionou</h2>
+      <p>Meu primerio parágrafo em reactjs</p>
     </div>
   )
 }
