bb4ca1ed2bb3536e92e20b0d92a82cb733919e39
NFPereira <nilsonfpereira@hotmail.com>
2023-11-02 11:42:46 -0300

    Inserindo imagem em assets

--- REACTJS/React_Do_Zero_A_Maestria/Arquivos/3_AVANCANDO_EM_REACT/avancando-em-react/src/App.jsx
+++ REACTJS/React_Do_Zero_A_Maestria/Arquivos/3_AVANCANDO_EM_REACT/avancando-em-react/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+
+//CSS
 import './App.css'
+
+import City from './assets/city.jpg';
 
 const App = () => {
   return (
     <>
     <div><h1>Avançando em React</h1></div>
+    {/* imagem em public */}
     <div>
       <img src="/imagem1.jpeg" alt="Windows" width="100%" />
     </div>
+    {/* imagem em assets */}
+    <img src={City} alt="Cidade" width="100%"/>
     </>
   )
 }
