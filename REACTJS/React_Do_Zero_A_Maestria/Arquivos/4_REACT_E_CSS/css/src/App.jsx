46d783345f73c87a3d075c364ab06701557e3074
NFPereira <nilsonfpereira@hotmail.com>
2023-11-03 23:04:57 -0300

    css inline e css inline dinâmico

--- REACTJS/React_Do_Zero_A_Maestria/Arquivos/4_REACT_E_CSS/css/src/App.jsx
+++ REACTJS/React_Do_Zero_A_Maestria/Arquivos/4_REACT_E_CSS/css/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import MyComponents from './components/MyComponents'
 
 function App() {
+  const n = 10;
+  const [name] = useState("Nilson")
 
   return (
     <>
@@ -14,6 +16,13 @@ function App() {
         {/* CSS de components */}
         <MyComponents />
         <p>Este parágrafo é do App.jsx</p>
+        {/* Inline css */}
+        <p style={{ color: "red", padding: "25px", borderTop: "2px solid teal" }}>Este elemento foi estilizado de forma inline</p>
+
+        {/* CSS Inline dinamico */}
+        <h2 style={n < 10 ? ({ color: "green" }) : ({ color: "#ffbb33" })}>Css style dinâmico</h2>
+
+        <h2 style={name === "Nilson" ? ({color:"#fff", background:"red"}) : ({color: "#000", background: "blue"})}>{name}</h2>
       </div>
     </>
   )
