6d6bab1578ca52d214d33fba5da05836a1092fbc
NFPereira <nilsonfpereira@hotmail.com>
2023-11-12 20:24:00 -0300

    Criação do projeto miniblog

--- REACTJS/React_Do_Zero_A_Maestria/Arquivos/9_CONTEXT_API/contextapi/src/pages/Products/index.jsx
+++ REACTJS/React_Do_Zero_A_Maestria/Arquivos/9_CONTEXT_API/contextapi/src/pages/Products/index.jsx
@@ -4,14 +4,14 @@ import { useCounterContext } from '../../hooks/useCounterContext'
 import { useTitleColorContext } from '../../hooks/useTitleColorContext';
 
 const Products = () => {
-   const {counter} = useCounterContext();
-   const {color} = useTitleColorContext();
-  return (
-    <div>
-      <h1 style={{color:color}}>Products</h1>
-    <p>Valor do contador: {counter}</p>
-    </div>
-  )
+   const { counter } = useCounterContext();
+   const { color } = useTitleColorContext();
+   return (
+      <div>
+         <h1 style={{ color: color }}>Products</h1>
+         <p>Valor do contador: {counter}</p>
+      </div>
+   )
 }
 
 export default Products
