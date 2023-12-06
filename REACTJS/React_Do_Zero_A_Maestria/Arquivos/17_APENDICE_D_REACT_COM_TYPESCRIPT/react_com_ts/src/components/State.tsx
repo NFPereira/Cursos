85b6a1c634185de99e0f3d2882b2a8f6609c5b40
NFPereira <nilsonfpereira@hotmail.com>
2023-12-06 20:14:04 -0300

    removendo importação não utilizada

--- REACTJS/React_Do_Zero_A_Maestria/Arquivos/17_APENDICE_D_REACT_COM_TYPESCRIPT/react_com_ts/src/components/State.tsx
+++ REACTJS/React_Do_Zero_A_Maestria/Arquivos/17_APENDICE_D_REACT_COM_TYPESCRIPT/react_com_ts/src/components/State.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent } from "react";
 
 const State = () => {
   const [text, setText] = useState<string | null>(null);
